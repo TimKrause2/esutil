1ea6d1026aef98e141884a83c7b0284c23560907
Timothy Krause <tmkrs4482@gmail.com>
2023-12-18 20:38:14 -0700

    Changed the minification to linear.

--- txquad.cpp
+++ txquad.cpp
@@ -82,7 +82,7 @@ void TexturedQuad::InitQuad(void)
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
     glTexParameteri( GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_FALSE );
 
     quad_initialized = true;
